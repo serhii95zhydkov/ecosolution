ffd8febe3c7409c4068017393caa8cbac67ea178
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-12-03 23:09:15 +0200

    update Electricity

--- src/components/Electricity/Electricity.jsx
+++ src/components/Electricity/Electricity.jsx
@@ -1,11 +1,36 @@
+import { useState, useEffect } from "react";
+
+import Container from "../../shared/Container/Container";
+
+import css from "./electricity.module.css";
+
 const Electricity = () => {
+  const [counter, setCounter] = useState(1134147814);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCounter((prevCounter) => prevCounter + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatNumberWithDots = (number) => {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
+
   return (
-    <section>
-      <div>
-        <h2>Electricity we produced for all time</h2>
-        {/* Counter */}
+    <Container>
+      <div className={css.section}>
+        <div className={css.container__title}>
+          <h2 className={css.title}>Electricity we produced for all time</h2>
+        </div>
+        <div className={css.container__number}>
+          <p className={css.number}>{formatNumberWithDots(counter)}</p>
+          <p className={css.text}>kWh</p>
+        </div>
       </div>
-    </section>
+    </Container>
   );
 };
 
